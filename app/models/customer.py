55b35c68c040da57d890764c2ee5b86c00eb5230
GraceOnwere <graceonwere.ng@gmail.com>
2026-09-07 16:48:20 +0100

    added customer class

---
+++ app/models/customer.py
@@ -0,0 +1,18 @@
+import uuid
+from uuid import UUID
+
+from pydantic import EmailStr
+from sqlmodel import SQLModel ,Field
+
+from app.models.enums.role import Role
+
+class Customer(SQLModel,table=True):
+    id: UUID = Field(default_factory=uuid.uuid4,primary_key=True)
+    full_name : str
+    username : str = Field(unique=True)
+    email : EmailStr = Field(unique=True)
+    password : str
+    role : Role
+    is_logged_in: bool = False
+    phone_number : str
+    address : str
